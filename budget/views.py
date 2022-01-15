b3b09751c56051b05d98b4ef805481c69ee848aa
Tomz899 <86239813+Tomz899@users.noreply.github.com>
2022-01-16 00:27:01 +0100

    Working with templates and queries

--- budget/views.py
+++ budget/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from .models import BudgetData
+from django.db.models import Sum
 
 
 def budget(request):
     expense_items = BudgetData.objects.filter(user_expense=request.user).order_by(
         "-date_added"
     )
+
+    total_cost = BudgetData.objects.filter(user_expense=request.user).aggregate(
+        Sum("cost")
+    )
+
     return render(
         request,
         "budget/budget.html",
-        context={"user": request.user, "expense_items": expense_items},
+        context={
+            "user": request.user,
+            "expense_items": expense_items,
+            "total_cost": total_cost,
+        },
     )
