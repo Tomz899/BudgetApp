f548d179a71153dd68998e2a7962ceac5070ed04
Tomz899 <tomek.nowak@aol.pl>
2022-05-06 14:01:33 +0200

    Added pagination for budget items

--- budget/views.py
+++ budget/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.shortcuts import render, redirect, get_object_or_404
-from django.db.models import Sum
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Sum
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.utils import timezone
-from .models import BudgetData
+
 from .forms import AddItem
+from .models import BudgetData
 
 
 @login_required
@@ -15,6 +17,11 @@ def budget(request):
         "-date_added"
     )
 
+    # Setting up pagination per expense_items
+    paginator = Paginator(expense_items, 10)  # Show 10 items per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     total_cost = BudgetData.objects.filter(user_expense=request.user).aggregate(
         Sum("cost")
     )
@@ -22,7 +29,7 @@ def budget(request):
     # AddItem{category, cost} form
     form = AddItem(request.POST)
     # form = AddItem(use_required_attribute=False) # ignores the field required text
-    
+
     if form.is_valid():
         form = form.save(commit=False)
         form.user_expense = request.user
@@ -38,6 +45,7 @@ def budget(request):
                 "expense_items": expense_items,
                 "total_cost": total_cost,
                 "form": form,
+                "page_obj": page_obj,
             },
         )
 
